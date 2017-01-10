52b003d439a86ebf3e3a66108a10749f6418ccee
Savio Joanes <sjoanes@uwaterloo.ca>
2017-01-10 18:16:53 -0500

    Insert HTML modal

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
-var iFrame = document.createElement ("iframe");
-iFrame.src = chrome.extension.getURL ("app.html");
-document.body.insertBefore (iFrame, document.body.firstChild);
+var iFrame = document.createElement("iframe");
+iFrame.src = chrome.extension.getURL("app.html");
+iFrame.setAttribute("style","position: absolute; left: 0px; top: 0px; background-color: rgb(255, 255, 255); opacity: 0.5; z-index: 2000; height: 1083px; width: 100%;");
+document.body.insertBefore(iFrame, document.body.firstChild);

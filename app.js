4fada0a21b3fa995d0e6ff0d45a8b142989255c1
Savio Joanes <sjoanes@uwaterloo.ca>
2017-01-17 17:51:08 -0500

    Inject with div

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ var iFrame = document.createElement("iframe");
 iFrame.src = chrome.extension.getURL("app.html");
 iFrame.setAttribute("style","position: absolute; left: 0px; top: 0px; background-color: rgb(255, 255, 255); opacity: 0.5; z-index: 2000; height: 1083px; width: 100%;");
 document.body.insertBefore(iFrame, document.body.firstChild);
+
+var link = document.createElement("link");
+link.href = chrome.extension.getURL("style.css");
+link.type = "text/css";
+link.rel = "stylesheet";
+iFrame.contentDocument.head.appendChild(link);
+
+console.log(3)

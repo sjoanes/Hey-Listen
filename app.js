55b965038ada0af76698d9ae86ff761c233fa58c
Savio <joanes.savio@gmail.com>
2017-01-18 14:30:52 -0500

    Dumb keyboard input multiple choice

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ var xhttp = new XMLHttpRequest();
 xhttp.open("GET", chrome.extension.getURL("app.html"), false);
 xhttp.send();
 wrapper.innerHTML = xhttp.responseText;
-wrapper.setAttribute("style","position: absolute; left: 0px; top: 0px; background-color: rgb(255, 255, 255); opacity: 0.5; z-index: 2000; height: 1083px; width: 100%;");
+wrapper.setAttribute("id", "hey-listen")
+wrapper.setAttribute("style",
+	"position: absolute;" + 
+	" left: 0px; top: 0px;" +
+	" background-color: rgb(255, 255, 255); " +
+	" z-index: 2000;" +
+	" height: 1083px;" +
+	" width: 100%;");
 document.body.insertBefore(wrapper, document.body.firstChild);
 
 var link = document.createElement("link");
@@ -12,4 +19,12 @@ link.type = "text/css";
 link.rel = "stylesheet";
 document.head.appendChild(link);
 
-console.log(3)
+document.addEventListener("keypress", function(event) {
+	// a = 97
+	// s = 115
+	// d = 100
+	// f = 102
+	if (event.keyCode === 97) {
+		document.getElementById("hey-listen").style.display = "none";
+	}
+});

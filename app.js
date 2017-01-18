8c00c3683a5adda8462b1c7af55135cc4aa2abbd
Savio <joanes.savio@gmail.com>
2017-01-18 13:59:32 -0500

    CSS and html loading into arbitrary page

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-var iFrame = document.createElement("iframe");
-iFrame.src = chrome.extension.getURL("app.html");
-iFrame.setAttribute("style","position: absolute; left: 0px; top: 0px; background-color: rgb(255, 255, 255); opacity: 0.5; z-index: 2000; height: 1083px; width: 100%;");
-document.body.insertBefore(iFrame, document.body.firstChild);
+var wrapper = document.createElement("div");
+var xhttp = new XMLHttpRequest();
+xhttp.open("GET", chrome.extension.getURL("app.html"), false);
+xhttp.send();
+wrapper.innerHTML = xhttp.responseText;
+wrapper.setAttribute("style","position: absolute; left: 0px; top: 0px; background-color: rgb(255, 255, 255); opacity: 0.5; z-index: 2000; height: 1083px; width: 100%;");
+document.body.insertBefore(wrapper, document.body.firstChild);
 
 var link = document.createElement("link");
 link.href = chrome.extension.getURL("style.css");
 link.type = "text/css";
 link.rel = "stylesheet";
-iFrame.contentDocument.head.appendChild(link);
+document.head.appendChild(link);
 
 console.log(3)

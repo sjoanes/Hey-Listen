f0d21b19310bfc5453d68dc4e4aeaed7bf34d4e6
Savio Joanes <sjoanes@uwaterloo.ca>
2017-01-23 20:46:31 -0500

    Clean up CSS a bit

--- app.js
+++ app.js
@@ -6,13 +6,6 @@ xhttp.open("GET", chrome.extension.getURL("app.html"), false);
 xhttp.send();
 wrapper.innerHTML = xhttp.responseText;
 wrapper.setAttribute("id", "hey-listen")
-wrapper.setAttribute("style",
-	"position: absolute;" + 
-	" left: 0px; top: 0px;" +
-	" background-color: rgb(255, 255, 255); " +
-	" z-index: 2000;" +
-	" height: 1083px;" +
-	" width: 100%;");
 document.body.insertBefore(wrapper, document.body.firstChild);
 
 var link = document.createElement("link");
@@ -22,10 +15,11 @@ link.rel = "stylesheet";
 document.head.appendChild(link);
 
 document.addEventListener("keypress", function(event) {
-	var adjusted = event.keyCode === 97 ? 0 :     	// a
-				   event.keyCode === 115 ? 1 : 		// s
-				   event.keyCode === 100 ? 2 : 		// d
-				   event.keyCode === 102 ? 3 : 4;	// f (4 is never the answer)
+	var key = event.keyCode || event.charCode; // cross-browser
+	var adjusted = key === 97 ? 0 :     	// a
+				   key === 115 ? 1 : 		// s
+				   key === 100 ? 2 : 		// d
+				   key === 102 ? 3 : 4;		// f (4 is never the answer)
 	if (adjusted === answer.option) {
 		document.getElementById("hey-listen").style.display = "none";
 	}
